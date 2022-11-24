32ee16ed02080e3de98ba448781563b2ab2b547e
Eirik Vullum <evullum@gmail.com>
2022-11-24 13:31:58 +0100

    Updates

---
+++ precache-manifest.a048bf4e6669580d5f07819dbd27836b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86552b1166a19f80bf2fb02cb7dcdb79",
+    "url": "/snow-drop/index.html"
+  },
+  {
+    "revision": "cd9f635fe8f5bad86cfc",
+    "url": "/snow-drop/static/css/main.5e32977d.chunk.css"
+  },
+  {
+    "revision": "86dcb2b6f6f81c09710b",
+    "url": "/snow-drop/static/js/2.adef8ef0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/snow-drop/static/js/2.adef8ef0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd9f635fe8f5bad86cfc",
+    "url": "/snow-drop/static/js/main.bd44b1c3.chunk.js"
+  },
+  {
+    "revision": "a867016c32e4726c152a",
+    "url": "/snow-drop/static/js/runtime-main.c99a6bef.js"
+  }
+]);
